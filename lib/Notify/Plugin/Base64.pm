b2b1545e04567340afc81d1c89215c926865d248
Tooru Iwasaki <rockbone.g@gmail.com>
2013-05-31 00:06:03 +0900

    automatically detect MIME type by MIME::Types

--- lib/Notify/Plugin/Base64.pm
+++ lib/Notify/Plugin/Base64.pm
@@ -3,6 +3,7 @@ package Notify::Plugin::Base64;
 use strict;
 use warnings;
 use MIME::Base64;
+use MIME::Types qw/by_suffix/;
 
 our $VERSION = 0.01;
 our $TYPE    = 'filter';
@@ -10,7 +11,9 @@ our $TYPE    = 'filter';
 sub hook {
     my ( $IN,$opt,$note ) = @_;
     $note->{base64} = 1; # flag for next phase
-    @$note{qw/filename type/} = @$opt{qw/filename type/};
+    $note->{filename} = $opt->{filename}
+        or die "Require filename";
+    ( $note->{type} ) = $opt->{type} || by_suffix( $opt->{filename} );
     $$IN = encode_base64( $$IN );
 }
 1;
