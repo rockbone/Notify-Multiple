cbc3677527f9a8e392da5eb2fca08863ee0ec5bb
Tooru Iwasaki <rockbone.g@gmail.com>
2013-06-18 22:53:51 +0900

    so not work `growl_running`

--- lib/Notify/Plugin/Growl.pm
+++ lib/Notify/Plugin/Growl.pm
@@ -9,8 +9,8 @@ our $TYPE = 'notify';
 # icon => icon path
 sub hook {
     my ( $IN,$arg,$note ) = @_;
-    if ( !growl_installed() || !growl_running() ){
-        die "Growl is not running\n";
+    if ( !growl_installed() ){
+        die "Growl is not installed\n";
     }
     my %register = (
         app     => $arg->{app} || 'NotifyMultiple',
